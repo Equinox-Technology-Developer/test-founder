dafa906c6fa556927ce0687654dc3cf33e407ff4
ermiee7 <yuhariermy@gmail.com>
2023-12-08 15:38:35 +0700

    create initial folder

---
+++ pages/pricing/index.js
@@ -0,0 +1,14 @@
+import { Layout } from '@/components'
+import React from 'react'
+
+import styles from './Pricing.module.scss'
+
+const Pricing = () => {
+  return (
+    <Layout pageTitle="Pricing"> 
+
+    </Layout>
+  )
+}
+
+export default Pricing
---
+++ pages/products/index.js
@@ -0,0 +1,14 @@
+import { Layout } from '@/components'
+import React from 'react'
+
+import styles from './Products.module.scss'
+
+const Products = () => {
+  return (
+    <Layout pageTitle="Products">
+
+    </Layout>
+  )
+}
+
+export default Products
