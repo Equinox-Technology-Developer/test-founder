909795547ce96b8011504c93d4d57084f7c80b4d
ermiee7 <yuhariermy@gmail.com>
2023-12-08 15:37:10 +0700

    re-create navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,14 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 import Image from "next/image";
 import Link from "next/link";
+
 import { useRouter } from "next/router";
-
 import { images } from "../../constants";
-
-import { FaBell } from "react-icons/fa";
-import { CgProfile } from "react-icons/cg";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 
 import styles from "./Navbar.module.scss";
@@ -17,14 +14,33 @@ const Navbar = () => {
   const router = useRouter();
   const [toogle, setToogle] = useState(false);
 
+  const handleScroll = () => {
+    const navbar = document.getElementById('navbar');
+    if (navbar) {
+      const top = window.scrollY;
+      if (top > 0) {
+        navbar.classList.add(styles.scrolled);
+      } else {
+        navbar.classList.remove(styles.scrolled);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav id="navbar" className={`${styles.navbar} ${toogle ? styles.toogleActive : ''}`}>
       <div className={styles.app__navbarLogo}>
         {/* for image on navbar */}
         <Link href="/">
           <Image
-            src={images.blueboxlogo2}
-            width={250}
+            src={images.logo_desktop}
+            width={120}
             height={50}
             alt="logo"
             priority
@@ -33,58 +49,50 @@ const Navbar = () => {
       </div>
       <div className={styles.nav_content}>
         <ul className={styles.app__navbarLinks}>
+          {/* Products */}
           <li>
             <Link
-              href="/"
+              href="/products"
               className={
-                router.pathname == "/" ? styles.active : styles.nonActive
+                router.pathname == "/products" ? styles.active : styles.nonActive
               }
             >
               {" "}
+              Product
+            </Link>
+          </li>
+          {/* Pricing */}
+          <li>
+            <Link
+              href="/pricing"
+              className={
+                router.pathname == "/pricing" ? styles.active : styles.nonActive
+              }
+            >
               Pricing
             </Link>
           </li>
+          {/* Resources */}
           <li>
             <Link
-              href="/"
+              href="/resources"
               className={
-                router.pathname == "#"
-                  ? styles.active
-                  : styles.nonActive
-              }
-            >
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className={
-                router.pathname == "#"
-                  ? styles.active
-                  : styles.nonActive
+                router.pathname == "/resources" ? styles.active : styles.nonActive
               }
             >
               Resources
             </Link>
           </li>
-          <li>
-            <Link
-              href="#"
-              className={
-                router.pathname == "#" ? styles.active : styles.nonActive
-              }
-            >
-              Contact Us
-            </Link>
-          </li>
         </ul>
-
-        {/* <div className={styles.nav_profile}>
-          <FaBell />
-          <CgProfile />
-        </div> */}
       </div>
+        <div class="flex justify-center">
+          <button class="inline-flex text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Book a Demo
+          </button>
+          <button class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
+            Try for free!
+          </button>
+        </div>
       <div className={styles.navbar_menu}>
         <HiMenuAlt4 onClick={() => setToogle(true)} />
         {toogle && (
