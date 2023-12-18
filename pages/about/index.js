7c43d41fcb26825f93f7641024857b191161f101
ermiee7 <yuhariermy@gmail.com>
2023-12-18 16:30:30 +0700

    modified about to default

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,36 +1,36 @@
 import { Layout } from '@/components';
 import React from 'react';
-import { fetchContentfulEntries } from '@/helper/contenfulHelper';
+// import { fetchContentfulEntries } from '@/helper/contenfulHelper';
 
 const About = ({ resources }) => {
   return <Layout pageTitle="About"></Layout>;
 };
 
-export async function getStaticProps() {
-  const contentType = 'landingPage'; // Modify the content type here
-  const propsKey = 'resources'; // Modify the props key here
-  const catchKey = 'error'; // Modify the catch key here
-  const indexToRead = 9; // Modify the index you want to read
+// export async function getStaticProps() {
+//   const contentType = 'landingPage'; // Modify the content type here
+//   const propsKey = 'resources'; // Modify the props key here
+//   const catchKey = 'error'; // Modify the catch key here
+//   const indexToRead = 9; // Modify the index you want to read
 
-  try {
-    const dynamicData = await fetchContentfulEntries(
-      contentType,
-      propsKey,
-      catchKey,
-      indexToRead,
-    );
+//   try {
+//     const dynamicData = await fetchContentfulEntries(
+//       contentType,
+//       propsKey,
+//       catchKey,
+//       indexToRead,
+//     );
 
-    return {
-      props: dynamicData,
-    };
-  } catch (error) {
-    console.error('Error in getStaticProps:', error);
-    return {
-      props: {
-        [catchKey]: 'An unexpected error occurred.',
-      },
-    };
-  }
-}
+//     return {
+//       props: dynamicData,
+//     };
+//   } catch (error) {
+//     console.error('Error in getStaticProps:', error);
+//     return {
+//       props: {
+//         [catchKey]: 'An unexpected error occurred.',
+//       },
+//     };
+//   }
+// }
 
 export default About;
