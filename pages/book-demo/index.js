f4723da5fb0e5dced9d92168012b7bbbea87cf61
muhreynaldii <reynaldi180101@gmail.com>
2024-01-04 14:58:44 +0700

    create tablet responsive and mobile responsive book a demo page

--- pages/book-demo/index.js
+++ pages/book-demo/index.js
@@ -1,9 +1,6 @@
 import { Layout } from '@/components';
 import React from 'react';
 import Image from 'next/image';
-import { images } from '@/constants';
-
-import styles from './BookDemo.module.scss';
 
 const BookDemo = ({}) => {
   return (
@@ -12,9 +9,9 @@ const BookDemo = ({}) => {
         <section className="bg-[#F9F9F9] bg-blur bg-right bg-no-repeat pt-0">
           <div className="container mx-auto pb-20">
             {' '}
-            <div className={` ${styles.banner_topContent}`}>
-              <div className={styles.banner_topContentText}>
-                <h1 className="sm:heading-1 heading-2 mb-6 w-full text-center lg:text-start">
+            <div className="flex w-full flex-col items-center justify-between lg:flex-row lg:px-0 lg:pb-[80px] lg:pt-[150px]">
+              <div className="flex w-full max-w-[646px] flex-col items-start px-4 text-left">
+                <h1 className="sm:heading-1 heading-2 mb-6 mt-[70px] w-full text-center md:mt-[100px] lg:mt-0 lg:text-start">
                   Objective hiring starts here. Book your demo now.
                 </h1>
                 <p className="caption-regular-3 sm:caption-regular-1 mb-6 mt-0 text-center lg:text-start">
@@ -30,7 +27,7 @@ const BookDemo = ({}) => {
                   streamline their hiring processes by reducing mis-hires and
                   cutting costs.
                 </p>
-                <div className="flex flex-row items-center gap-4">
+                <div className="mb-6 flex flex-row flex-wrap items-center justify-center gap-4 md:mb-[60px] lg:mb-0 lg:flex-nowrap lg:justify-start">
                   <Image
                     src="/assets/abbot.svg"
                     width="119"
@@ -63,11 +60,9 @@ const BookDemo = ({}) => {
                   />
                 </div>
               </div>
-              <div
-                className={`flex w-full justify-center md:w-full lg:w-full lg:max-w-lg ${styles.banner_topContentImage}`}
-              >
-                <form className="h-full w-[580px] rounded-[20px] bg-shade-0 px-[42px] py-[32px] shadow-[0_4px_10px_0px_rgba(0,0,0,0.15)]">
-                  <div className="flex flex-col gap-6">
+              <div className="flex h-auto w-full justify-center px-4 md:w-full lg:min-h-[403px] lg:w-full lg:min-w-[609px] lg:max-w-lg lg:px-0">
+                <form className="h-full w-full rounded-[20px] bg-shade-0 px-[42px] py-[32px] shadow-[0_4px_10px_0px_rgba(0,0,0,0.15)] md:w-[580px] ">
+                  <div className="flex flex-col gap-6 ">
                     <p className="caption-regular-2 flex w-full justify-end gap-2">
                       Not you? <span>Click here to reset</span>
                     </p>
@@ -89,33 +84,33 @@ const BookDemo = ({}) => {
                         className="h-[48px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]"
                       />
                     </div>
-                    <div className="flex items-start justify-between gap-6">
-                      <div className="flex w-full max-w-[240px] flex-col">
+                    <div className="flex items-end justify-between gap-6 ">
+                      <div className="flex w-1/2 flex-col md:w-full">
                         <label className="caption-regular-4 text-[#CBCBCB]">
                           Job function*
                         </label>
                         <input
                           type="email"
-                          className="h-[48px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]"
+                          className="h-[48px] w-full rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]"
                         />
                       </div>
-                      <div className="flex w-full max-w-[240px] flex-col">
+                      <div className="flex w-1/2 flex-col md:w-full">
                         <label className="caption-regular-4 text-[#CBCBCB]">
                           Company name*
                         </label>
                         <input
                           type="text"
-                          className="h-[48px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]"
+                          className="h-[48px] w-full rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]"
                         />
                       </div>
                     </div>
-                    <div className="flex items-start justify-between gap-6">
-                      <div className="flex w-full max-w-[240px] flex-col">
+                    <div className="flex items-end justify-between gap-6">
+                      <div className="flex w-full flex-col">
                         <label className="caption-regular-4 text-[#CBCBCB]">
                           How many people work in your organization (Full Time
                           Equivalent)?*
                         </label>
-                        <select className="h-[48px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]">
+                        <select className="h-[48px] w-full rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]">
                           <option value="DEFAULT" disabled selected hidden>
                             FTE
                           </option>
@@ -125,11 +120,11 @@ const BookDemo = ({}) => {
                           <option value="51-100">51-100</option>
                         </select>
                       </div>
-                      <div className="flex w-full max-w-[240px] flex-col">
+                      <div className="flex w-full flex-col">
                         <label className="caption-regular-4 text-[#CBCBCB]">
                           How many people are you planning to hire next year?*
                         </label>
-                        <select className="h-[48px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]">
+                        <select className="h-[48px] w-full rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]">
                           <option value="DEFAULT" disabled selected hidden>
                             Please Select
                           </option>
