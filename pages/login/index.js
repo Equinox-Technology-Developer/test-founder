607a713ec12da7a3662dc53a9f6f409a123b27ca
muhreynaldii <reynaldi180101@gmail.com>
2024-01-04 13:54:28 +0700

    responsive mobile and tablet for page login

--- pages/login/index.js
+++ pages/login/index.js
@@ -9,7 +9,7 @@ const Login = ({}) => {
   return (
     <>
       <main className="flex h-screen w-screen flex-col lg:flex-row">
-        <div className="flex w-full justify-center bg-grayscale-100 lg:w-1/2 lg:pl-[132px] lg:pr-[92px]">
+        <div className="flex min-h-screen w-full justify-center bg-grayscale-100 px-[30px] py-[60px] md:px-0 md:py-[64px] lg:w-1/2 lg:pl-[132px] lg:pr-[92px]">
           <div className="flex w-[508px] flex-col items-start justify-center space-y-6">
             <Link href="/">
               <Image
@@ -20,8 +20,10 @@ const Login = ({}) => {
                 priority
               />
             </Link>
-            <h3 className="heading-3">We are glad to see you again.</h3>
-            <button className="caption-semibold-3 flex h-[48px] w-full items-center justify-center gap-[12px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]">
+            <h3 className="md:heading-3 text-[14px] font-bold leading-[16px]">
+              We are glad to see you again.
+            </h3>
+            <button className="md:caption-regular-3 flex h-[48px] w-full items-center justify-center gap-[12px] rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px] text-xs leading-[24px]">
               <FcGoogle className="h-6 w-6" />
               Continue for Google
             </button>
@@ -40,13 +42,13 @@ const Login = ({}) => {
               className="h-[48px] w-full rounded-[5px] border-[0.75px] border-[#CBCBCB] bg-shade-0 px-4 py-[10px]"
               placeholder="Password"
             />
-            <div className="flex w-full justify-between">
+            <div className="flex w-full items-center justify-between">
               <div className="flex items-center gap-[6px]">
                 <input type="checkbox" className="accent-primary-500" />
                 <span className="caption-regular-3">Keep me logged</span>
               </div>
               <Link href="/forgot-password">
-                <p className="text-xs text-black">Set or Reset Password</p>
+                <p className="text-xs text-black ">Set or Reset Password</p>
               </Link>
             </div>
             <button className="caption-semibold-1 h-[48px] w-full rounded-[40px] bg-primary-500 px-[20px] py-[8px] text-center text-shade-0">
@@ -68,7 +70,7 @@ const Login = ({}) => {
             </p>
           </div>
         </div>
-        <div className="flex w-full flex-col items-center justify-center space-y-[60px] bg-primary-500 px-[120px] lg:w-1/2">
+        <div className="hidden w-full flex-col items-center justify-center space-y-[60px] bg-primary-500 px-[120px] lg:flex lg:w-1/2">
           <h1 className="text-center text-[60px] font-bold leading-[60px] tracking-[1%] text-shade-0">
             Is this the end of the resume
           </h1>
