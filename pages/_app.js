19f9aa35ec80a12e38e08e1c682d04052e896d6e
ermiee7 <yuhariermy@gmail.com>
2024-01-24 17:37:04 +0700

    add meta for google analytics, and vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 import { Open_Sans } from 'next/font/google';
 
 import '@/styles/globals.css';
@@ -12,7 +13,6 @@ const openSans = Open_Sans({
 export default function App({ Component, pageProps }) {
   return (
     <main className={openSans.className}>
-      
       <Head>
         <meta name="description" content="Test Founder" />
         <meta
@@ -22,10 +22,12 @@ export default function App({ Component, pageProps }) {
         />
         <meta property="og:title" content="Test Founder" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="google-site-verification" content="h_Vgb8BndmNa_Jn43uDPKrvlqioQbhRJnXPwo1eHW24" />
         <link rel="icon" href="/assets/favicon.ico" />
       </Head>
-
-      <Component {...pageProps} />
+      
+      <Analytics/>
+      <Component {...pageProps} />    
     </main>
   );
 }
