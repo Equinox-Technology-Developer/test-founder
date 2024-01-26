7f454a556ec3c606d5e3e4c07c80278dada237fc
ermiee7 <yuhariermy@gmail.com>
2024-01-26 15:21:13 +0700

    adding and fetching content from contentful

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,31 +1,31 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { FaTwitter, FaFacebookSquare, FaLinkedin   } from "react-icons/fa";
 
-import { fetchContentfulEntries } from '@/helper/accessContentful';
+import { FaTwitter, FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
+import { CgClose } from 'react-icons/cg';
 import { Layout } from '@/components';
+import { fetchContentfulEntries } from '@/lib/contentful/client';
 
-import styles from './Blog.module.scss'
+import styles from './Blog.module.scss';
 
-export default function Post(post) {
+export default function Post({ post, latestPosts, relatedPosts }) {
   const router = useRouter();
-  const articleItem = post.post.fields
-  console.log(articleItem);
+  const articleItem = post.fields;
+
   return (
     <Layout pageTitle={articleItem.title} key={router.query.slug}>
       {/* Top Menu - Category */}
       <section className="w-full bg-[#f5f6f6]">
         <div className="container mx-auto mt-28 p-4">
           <div className="flex w-full items-center justify-center gap-20">
-            <p className='cursor-pointer'>All</p>
-            <p className='cursor-pointer'>Hiring & Recruiting</p>
-            <p className='cursor-pointer'>Human Resources</p>
-            <p className='cursor-pointer'>Science</p>
-            <p className='cursor-pointer'>Skills-based Hiring</p>
-            <p className='cursor-pointer'>Talent Assesment</p>
-            <p className='cursor-pointer'>Test Founder</p>
+            <p className="cursor-pointer">All</p>
+            <p className="cursor-pointer">Hiring & Recruiting</p>
+            <p className="cursor-pointer">Human Resources</p>
+            <p className="cursor-pointer">Science</p>
+            <p className="cursor-pointer">Skills-based Hiring</p>
+            <p className="cursor-pointer">Talent Assesment</p>
+            <p className="cursor-pointer">Test Founder</p>
           </div>
         </div>
       </section>
@@ -33,26 +33,32 @@ export default function Post(post) {
       {/* Article - Blog articleItem */}
       <section key={articleItem}>
         <div className="container mx-auto p-5 md:p-12">
-          <div className="mx-auto gap-6 flex items-center justify-between w-full md:max-w-screen-xl">
-            {/* Content */}
-            <div className={`w-full flex-col md:min-w-min ${styles.blogPost}`}>
-              <h1 className='heading-3 md:heading-1 '>{articleItem.title}</h1>
-              <Image src={`https:${articleItem.featuredImage.fields.file.url}`}
-              width={articleItem.featuredImage.fields.file.details.image.width}
-              height={articleItem.featuredImage.fields.file.details.image.height}
-              alt={`Featured Image for ${articleItem.title}`}
-              className='img py-5' />
+          <div className="mx-auto flex w-full items-center justify-between md:gap-6 md:max-w-screen-xl">
+            {/* Left Content */}
+            <div>
+              {/* Main Content */}
+              <div className={`w-full flex-col md:min-w-min ${styles.blogPost}`}>
+                <h1 className="heading-3 md:heading-1 ">{articleItem.title}</h1>
+                <Image
+                  src={`https:${articleItem.featuredImage.fields.file.url}`}
+                  width={articleItem.featuredImage.fields.file.details.image.width}
+                  height={articleItem.featuredImage.fields.file.details.image.height}
+                  alt={`Featured Image for ${articleItem.title}`}
+                  className="img py-5"
+                />
 
-              <p className='caption-regular-3 my-2'>{articleItem.excerpt}</p>
+                <p className="caption-regular-3 my-2">{articleItem.excerpt}</p>
 
-              <div className='flex flex-col md:flex-row justify-between items-center'>
-                <div className='flex justify-between items-center gap-2'>
-                  <Image
-                  src={`https:${articleItem.author.fields.image.fields.file.url}`}
-                  width={articleItem.author.fields.image.fields.file.details.image.width}
-                  height={articleItem.author.fields.image.fields.file.details.image.height} />
-                  <p>{articleItem.author.fields.name}</p>
-                  <date className="caption-regular-4 ml-4">
+                <div className="flex flex-col items-center justify-between md:flex-row">
+                  <div className="flex items-center justify-between gap-2">
+                    <Image
+                      src={`https:${articleItem.author.fields.image.fields.file.url}`}
+                      width={articleItem.author.fields.image.fields.file.details.image.width
+                      }
+                      height={articleItem.author.fields.image.fields.file.details.image.height}
+                    />
+                    <p>{articleItem.author.fields.name}</p>
+                    <date className="caption-regular-4 ml-4">
                       {new Date(
                         articleItem.author.fields.image.sys.createdAt,
                       ).toLocaleDateString('en-US', {
@@ -61,25 +67,85 @@ export default function Post(post) {
                         day: 'numeric',
                       })}
                     </date>
+                  </div>
+                  <div className="flex  items-center justify-center gap-2">
+                    <FaLinkedin />
+                    <FaFacebookSquare />
+                    <FaTwitter />
+                  </div>
                 </div>
-                <div className='flex  justify-center items-center gap-2'>
-                  <FaLinkedin />
-                  <FaFacebookSquare />
-                  <FaTwitter />
+
+                <div>
+                  <p className="my-10">
+                    {articleItem.bodyContent.content[0].content[0].value}
+                  </p>
                 </div>
               </div>
-              
-              <div>
-                <p className='my-10'>{articleItem.bodyContent.content[0].content[0].value}</p>
+
+              {/* Related Post */}
+              <div className={styles.relatedPost_container}>
+                <h2 className="heading-2">Related Posts</h2>
+                <div className={styles.relatedPost_wrapper}>
+                  {/* Render related posts */}
+                  {relatedPosts.map((post) => (
+                    <Link href={`/blog/${post.fields.slug}`}>
+                      <Image src={`https:${post.fields.featuredImage.fields.file.url}`} 
+                      height={post.fields.featuredImage.fields.file.details.image.height}
+                      width={post.fields.featuredImage.fields.file.details.image.width} 
+                      alt={post.fields.internalName} />
+                      <p>{post.fields.title}</p>
+                    </Link>
+                  ))}
+                </div>
               </div>
-
             </div>
 
-            {/* Right Banner */}
-            <div className='md:block hidden'>
-              <h2 className='heading-2'>Lastest Post</h2>
-              
+            {/* Right Content */}
+            <div className="hidden md:flex self-start w-full  flex-col gap-10">
+              <div className="relative flex w-full xl:w-fit ">
+                <input
+                  type="text"
+                  className="rounded-[10px] w-full bg-white px-4 py-[10px] shadow-[0_4px_10px_0px_rgba(0,0,0,0.15)] placeholder:text-base placeholder:text-[#CBCBCB] xl:w-[365px] "
+                  placeholder="Search anything here"
+                />
+                <Image
+                  src="/assets/icon-search.svg"
+                  alt="icon-search"
+                  width={20}
+                  height={20}
+                  sizes="100vw"
+                  className="absolute right-4 top-1/2 flex h-auto -translate-y-1/2"
+                />
+              </div>
+              <div className={styles.cards}>
+                <h2 className="heading-2">Latest Posts</h2>
+                  {/* Render latest posts */}
+                  <div className={styles.latestPosts_wrapper}>
+                    {latestPosts.map((post) => (
+                      <Link href={`/blog/${post.fields.slug}`}>
+                        <div className={styles.latestPosts_item}>
+                          <Image src={`https:${post.fields.featuredImage.fields.file.url}`} 
+                          height={post.fields.featuredImage.fields.file.details.image.height}
+                          width={post.fields.featuredImage.fields.file.details.image.width} 
+                          alt={post.fields.internalName} />
+                          <p>{post.fields.title}</p>
+                        </div>
+                    </Link>
+                    ))}
+                  </div>
+              </div>
+              {/* Subscription */}
+              {/* <div className={styles.cards}>
+                <h3 className="heading-3">The best advice in pre-employment testing, in your inbox.</h3>
+                <p>No spam. Unsubscribe at any time</p>      
+              </div> */}
+
+              {/* Sticky Banner Registration */}
+              {/* <div></div> */}
             </div>
+
+            {/* Close Button */}
+            {/* <CgClose /> */}
           </div>
         </div>
       </section>
@@ -108,6 +174,15 @@ export const getStaticProps = async ({ params }) => {
 
     const post = items.find((post) => post.fields.slug === slug);
 
+    // Logic to fetch latest posts
+    const latestPosts = items.slice(0, 5); // Example: Get first 5 posts as latest
+
+    // Logic to fetch related posts
+    const relatedPosts = items.filter(
+      (p) =>
+        p.fields.category === post.fields.category && p.fields.slug !== slug,
+    ).slice(0,3);
+
     if (!post) {
       return {
         redirect: {
@@ -120,6 +195,8 @@ export const getStaticProps = async ({ params }) => {
     return {
       props: {
         post,
+        latestPosts,
+        relatedPosts,
       },
     };
   } catch (error) {
@@ -128,6 +205,8 @@ export const getStaticProps = async ({ params }) => {
     return {
       props: {
         post: null,
+        latestPosts: [],
+        relatedPosts: [],
       },
     };
   }
