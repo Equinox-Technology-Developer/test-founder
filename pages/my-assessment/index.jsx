2a379a32febdca9b413b7da8f4b86c1d1028bbb9
muhreynaldii <reynaldi180101@gmail.com>
2024-04-22 14:18:28 +0700

    complete section in my candidates

--- pages/my-assessment/index.jsx
+++ pages/my-assessment/index.jsx
@@ -2,7 +2,7 @@ import { Layout } from '@/components';
 import { FiPlusCircle } from 'react-icons/fi';
 import Image from 'next/image';
 
-export default function Assessments({}) {
+export default function MyAssessments({}) {
   return (
     <>
       <main className="h-screen max-h-screen overflow-hidden bg-[#F9F9F9]">
@@ -28,7 +28,7 @@ export default function Assessments({}) {
                     placeholder="Search any assessment"
                     className="mt-4 w-[425px] rounded-[5px] border border-gray-300 px-4 py-2"
                   />
-                  <select className="appearance-none bg-[url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23131313%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')] bg-[length:10px] bg-[right_1.7rem] bg-no-repeat px-4 py-2">
+                  <select className="text-neutral-100px-4 max-h-[48px] rounded-[5px] border border-gray-300 px-4 py-2">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="in-progress">In Progress</option>
