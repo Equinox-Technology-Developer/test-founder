307da3cb0c9db2c41884040cd2b2344f3714e851
muhreynaldii <reynaldi180101@gmail.com>
2024-04-25 17:36:51 +0700

    create pop up extra time approved

---
+++ components/CandidateTest/ExtraTime.jsx
@@ -0,0 +1,25 @@
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+export default function ExtraTime() {
+  return (
+    <>
+      <div className="grid h-fit max-w-[674px] grid-cols-1 gap-x-[24px] rounded-[20px] bg-white p-[42px] shadow-[0_4px_10px_0px_rgba(0,0,0,0.15)]">
+        <div className="space-y-[32px]">
+          <h2 className="heading-3 border-b-[1px] border-black pb-3">
+            Extra time approved
+          </h2>
+          <p className="caption-regular-3">
+            Based on the details you gave while setting up your assessment, you
+            have been granted 50% extra time.
+          </p>
+          <div className="flex justify-end">
+            <button className="caption-semibold-1 flex items-center rounded-[40px] bg-primary-500 px-[20px] py-[8px] text-white hover:bg-primary-600">
+              Continue
+              <MdOutlineKeyboardArrowRight />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
