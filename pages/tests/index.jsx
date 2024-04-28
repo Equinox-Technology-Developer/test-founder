fe927f2dc896da6f446856439bb96d4b13b1fb21
muhreynaldii <reynaldi180101@gmail.com>
2024-04-28 14:14:31 +0700

    update banner section 1 homepage

--- pages/tests/index.jsx
+++ pages/tests/index.jsx
@@ -3,7 +3,7 @@ import NextTest from '@/components/CandidateTest/NextTest';
 export default function Tests({}) {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center bg-[#F9F9F9]">
+      <div className="flex h-screen w-screen flex-row items-center justify-center bg-[#F9F9F9]">
         <NextTest />
       </div>
     </>
